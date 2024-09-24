fc14a53525ac631feceb385977413cabdfb587ba
Bob Murmu <bobmurmu02@gmail.com>
2024-09-24 22:12:42 +0530

    pagination done

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -2,9 +2,21 @@ import prisma from "@/prisma/db";
 import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+interface SearchParms {
+  page: string;
+}
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParms }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const tickets = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+
+  const ticketCount = await prisma.ticket.count();
   return (
     <div>
       <Link
@@ -14,6 +26,11 @@ const Tickets = async () => {
         New Ticket
       </Link>
       <DataTable tickets={tickets} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
